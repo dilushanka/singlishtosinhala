f7e2a82c8a06f58a7171b8af79f0b2a5bb6256d0
Chathura Dilushanka <90453201+dilushanka@users.noreply.github.com>
2023-01-27 07:19:18 +0530

    Delete assets directory

--- assets/js/env.js
+++
@@ -1,7 +0,0 @@
-var base_url;var index_url;var images;var TESTING="test";var DEVELOPMENT="development";var PRODUCTION="production";var PAYHERE_PAYMENT_FEE=10;if(environment=='development')
-{base_url="http://localhost:8080/";index_url=base_url;images=base_url+'assets/images/'}
-else if(environment=='production')
-{base_url="index.html";index_url=base_url;images='/assets/images/'}
-else if(environment=='testing')
-{base_url="https://dev.bhasha.lk/helacon/lk-dev-ci4/";index_url=base_url;images=base_url+'assets/images/'}
-var FILTERCALL="https://helacon.lk/modules/dictionary/api/filter.php";var GETWORDCALL="https://helacon.lk/modules/dictionary/api/read.php";
