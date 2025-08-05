557dfda98a916effc338340e233abf32202cb150
Chathura Dilushanka <90453201+dilushanka@users.noreply.github.com>
2025-08-05 09:48:02 +0530

    UI UX Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,27 +1,3 @@
-var myEvents = [{
-	name: "New Year",
-	date: "Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
-	type: "holiday",
-	everyYear: true
-}, {
-	name: "Valentine's Day",
-	date: "Fri Feb 14 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
-	type: "holiday",
-	everyYear: true
-}, {
-	name: "Birthday",
-	date: "February/3/2020",
-	type: "birthday"
-}, {
-	name: "Author's Birthday",
-	date: "February/15/2020",
-	type: "birthday",
-	everyYear: true
-}, {
-	name: "Holiday",
-	date: "February/15/2020",
-	type: "event"
-}, ]
 var side_bar = $('#hela_side_nav');
 var logger = function() {
 	var oldConsoleLog = null;
